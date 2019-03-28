ba1960fa0dab5ff5057d52def4133f332f0afca0
Pantoji, Siddhiraj <siddhiraj.pantoji@travelex.com>
2019-03-28 17:52:02 +0530

    Changes for Starting and Ending Application

--- tests/features/login.feature
+++ tests/features/login.feature
@@ -9,8 +9,8 @@ Feature: Login
         Then I should get any responseStatus
         And I should get value in response <Status>
         Examples:
-            | Username                   | Password | Status |
-            | Sidd                       | Sidd@123 | 400    |
-            | siddhirajpantoji@gmail.com | Sidd@123 | 200    |
-            |                            |          | 400    |
+            | Username                     | Password   | Status |
+            | 'Sidd'                       | 'Sidd@123' | 400    |
+            | 'siddhirajpantoji@gmail.com' | 'Sidd@123' | 200    |
+            | ''                           | ''         | 400    |
 
--- tests/step-definitions/LoginSteps.js
+++ tests/step-definitions/LoginSteps.js
@@ -5,8 +5,45 @@ const {
     Then,
     After,
     Status,
-    setDefaultTimeout
+    setDefaultTimeout, 
+    BeforeAll,
+    AfterAll
 } = require('cucumber');
 
 var should = require('chai').should(),
     expect = require('chai').expect;
+
+  BeforeAll(()=>{
+    console.log("Application startup over here ")
+  })
+    Given('create a login request {string} {string}', function (string, string2) {
+        // Write code here that turns the phrase above into concrete actions
+        console.log("value of String 1 :"+ string);
+        console.log("value of String 2 :"+ string2);
+        var reqBody = {
+          username:string,
+          password:string2
+        }
+
+        return 'pending';
+      });
+
+      When('I hit endpoint with request', function (callback) {
+        // Write code here that turns the phrase above into concrete actions
+        return 'pending';
+      });
+
+      
+      Then('I should get any responseStatus', function () {
+        // Write code here that turns the phrase above into concrete actions
+        return 'pending';
+      });
+
+      Then('I should get value in response {int}', function (int) {
+          console.log("Value of Status ", int);
+        // Write code here that turns the phrase above into concrete actions
+        return 'pending';
+      });
+      AfterAll(()=>{
+        console.log("Application end over here ")
+      }) 
