3186f9087f146762a261050d12a0f3b4679fc2d2
Pantoji, Siddhiraj <siddhiraj.pantoji@travelex.com>
2019-04-08 15:42:36 +0530

    Changes for Github Commit URL

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 // First App that loads 
 require('dotenv').config();
-port = process.env.PORT;
-var app = require('./app')
+let port = process.env.PORT;
+var app = require('./app');
 
 var server  = app.listen(port, (err)=>{
     if(err){
-        console.error("Unable to start setup in port "+ port, err)
+        console.error('Unable to start setup in port '+ port, err);
     }
     else{
-        console.log("Server started at ", port);
+        console.log('Server started at ', port);
     }
-})
+});
 //console.log(server)
 // server.close(()=>{
 //     console.log("Server Closed ")
